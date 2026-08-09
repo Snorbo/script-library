8dfaee00ed82eabf7624d6f0daf310bee24b91a3
Snorbo <140411654+Snorbo@users.noreply.github.com>
2026-08-09 18:48:06 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -119,7 +119,8 @@ option1() {
 option2() {
     echo -e "${YELLOW}执行：启用 SSH 密钥连接...${NC}"
     bash <(curl -s https://raw.githubusercontent.com/Snorbo/script-library/refs/heads/main/sshkey.sh)
-    echo -e "${GREEN}完成。${NC}"
+    echo -e "${GREEN}完成，服务器即将重启${NC}"
+    reboot
     read -p "按回车键继续..."
 }
 
